809d95d7158996c7da9e5681b2ad701afed10496
Nick Kryuchkov <n9163293481@gmail.com>
2022-04-05 11:56:14 +0300

    Fill README and fix typos in script

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Easy WireGuard Server
-Scripts for quickly setting up your WireGuard server
+Scripts for quickly setting up your WireGuard server.
+
+## Add new client
+Download and run script with:
+```
+curl -O https://raw.githubusercontent.com/bllizard22/easy-wireguard-server/main/add_client.sh
+chmod +x add_client.sh
+./add_client.sh
+```
+You will be prompted to choose whether to output the result as QR-code or configuration file:
+
+`Is QR-code suitable for output? [y/n]`
+
+ QR-code is suitable for smartphones and tablets, for desktop devices it is better to choose config.
--- add_client.sh
+++ add_client.sh
@@ -8,7 +8,7 @@ NC='\033[0m'
 ### if config can be outputted as QR-code
 echo -en "${GREEN}Enter device name: ${NC}"
 read DEVICE_NAME
-echo -n "${GREEN}Is QR-code suitable for output? [y/n]${NC}"
+echo -en "${GREEN}Is QR-code suitable for output? [y/n]${NC}"
 read IS_QRCODE
 
 ### Create client keys
@@ -18,24 +18,23 @@ wg genkey | sudo tee /etc/wireguard/clients/$DEVICE_NAME.key | wg pubkey | sudo 
 ### Get constants for further actions 
 SERVER_PUBLIC=$(</etc/wireguard/server_public.key)
 IP_ADR=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
-line=$(grep -i "ListenPort" ~/desktop/wireguard/wg0.conf)
+line=$(grep -i "ListenPort" /etc//wireguard/wg0.conf)
 IFS='= '; port=($line); unset IFS; IP_PORT="$IP_ADR:${port[1]}"
 DEVICE_PUBLIC=$(</etc/wireguard/clients/$DEVICE_NAME.key.pub)
 DEVICE_PRIVATE=$(</etc/wireguard/clients/$DEVICE_NAME.key)
 line=$( tail -n 1 /etc/wireguard/wg0.conf )
 IFS='.'; last_ips=($line); unset IFS;
 last_ip=${last_ips[3]}; NEXT_IP=$((last_ip+=1))
-echo "Server public - $SERVER_PUBLIC"
-echo "Server IP:Port - $IP_PORT"
-echo "Device public - $DEVICE_PUBLIC"
-echo "Device private - $DEVICE_PRIVATE"
-echo "Next allowed IP - $NEXT_IP"
+echo "Server public - $SERVER_PUBLIC
+Device public - $DEVICE_PUBLIC
+Device private - $DEVICE_PRIVATE
+Server - $IP_PORT; Next allowed IP - x.x.x.$NEXT_IP"
 
 ### Create client config
 touch /etc/wireguard/clients/$DEVICE_NAME.conf
 echo "[Interface]
 PrivateKey = $DEVICE_PRIVATE
-Address = 10.18.0.2/32
+Address = 10.18.0.$NEXT_IP
 DNS = 8.8.8.8, 1.1.1.1, 1.0.0.1
 
 [Peer]
